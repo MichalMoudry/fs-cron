5d4b9f72ad62d1710896258dd49be8454ec67ca0
Michal Moudrý <moudry.michal@live.com>
2024-08-21 16:52:41 +0200

    remove the ICronExpression + updated scheduler

--- src/Scheduler.fs
+++ src/Scheduler.fs
@@ -2,28 +2,49 @@
 
 open System
 open System.Collections.Generic
+open System.Threading
+open System.Threading.Tasks
 open Cronos
 
 [<Sealed>]
 type Scheduler() =
     let jobs = List<JobDefinition>()
+    let mutable hasParallelExecution = false
 
-    let startBlocking() = ()
+    let startBlocking() =
+        while true do
+            printfn "Test"
+            Thread.Sleep(1_000)
 
     let startParallel() = ()
 
+    let startInternal() =
+        match hasParallelExecution with
+        | true -> startParallel()
+        | false -> startBlocking()
+
+    /// Method for adding a new job into a scheduler.
     member this.NewJob (cronDef: string) (job: Action) =
         jobs.Add(JobDefinition(
             CronExpression.Parse(cronDef),
             job
         ))
 
+    member this.NewAsyncJob (cronDef: string) (job: Task) =
+        ()
+
+    /// Method for initializing the scheduler and starting all its jobs.
     member this.Start isParallel =
-        match isParallel with
-        | true -> startBlocking()
-        | false -> startParallel()
-        (*let thread = Thread(ThreadStart(startInternal))
-        thread.IsBackground <- true
-        thread.Start()*)
+        if jobs.Count = 0 then ()
+        else
+            hasParallelExecution <- isParallel
+            let cancellationToken =
+                match isParallel with
+                | true -> CancellationToken.None
+                | false -> CancellationToken.None
+
+            let thread = Thread(ThreadStart(startInternal))
+            thread.IsBackground <- true
+            thread.Start()
 
     member this.Stop() = ()
--- test/FsCron.TestApp/Program.cs
+++ test/FsCron.TestApp/Program.cs
@@ -7,6 +7,10 @@ scheduler.NewJob("* * * * *", () =>
 {
     Console.WriteLine($"[{DateTimeOffset.Now}] Echo");
 });
+scheduler.NewJob("* * * * *", () =>
+{
+    Console.WriteLine($"[{DateTimeOffset.Now}] Echo 2");
+});
 scheduler.Start(false);
 
 Console.WriteLine("Press any key to exit...");
