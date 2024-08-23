1d2294d96ef33e48253981a29180d4c51400e690
Michal Moudrý <moudry.michal@live.com>
2024-08-23 17:35:21 +0200

    started sync api

--- src/JobDefinition.fs
+++ src/JobDefinition.fs
@@ -6,18 +6,21 @@ open System
 
 type IJobDefinition =
     abstract CurrentDate: DateTimeOffset
+    abstract Execute: unit -> Task<unit>
 
 [<Sealed>]
-type internal JobDefinition(cronExp: CronExpression, work: Action) =
-    let mutable date = DateTimeOffset.Now
-    member this.CurrentDate with get() = date
+type internal JobDefinition(cronExp: CronExpression, work: Task) =
+    let mutable date =
+        cronExp.GetNextOccurrence(DateTimeOffset.Now, TimeZoneInfo.Local).Value
 
-[<Sealed>]
-type internal AsyncJobDefinition(cronExp: CronExpression, work: Task) =
-    let mutable date = DateTimeOffset.Now
-    member this.CurrentDate with get() = date
-    member this.Exec() =
-        task {
-            do! work
-            date <- cronExp.GetNextOccurrence(date, TimeZoneInfo.Local).Value
-        }
+    interface IJobDefinition with
+        member this.CurrentDate = date
+        member this.Execute() =
+            task {
+                do! work
+                let next = cronExp.GetNextOccurrence(date, TimeZoneInfo.Local)
+                date <- if next.HasValue then next.Value else date
+            }
+
+    new(cronExp: CronExpression, action: Action) =
+        JobDefinition(cronExp, new Task(action))
--- src/Scheduler.fs
+++ src/Scheduler.fs
@@ -5,10 +5,11 @@ open System.Collections.Generic
 open System.Threading
 open System.Threading.Tasks
 open Cronos
+open Microsoft.FSharp.Control
 
 [<Sealed>]
 type Scheduler() =
-    let jobs = List<JobDefinition>()
+    let jobs = List<IJobDefinition>()
 
     /// Method for adding a new job into a scheduler.
     member this.NewJob (cronDef: string) (job: Action) =
@@ -18,12 +19,31 @@ type Scheduler() =
         ))
 
     member this.NewAsyncJob (cronDef: string) (job: Task) =
-        ()
+        jobs.Add(JobDefinition(
+            CronExpression.Parse(cronDef),
+            job
+        ))
 
     /// Method for initializing the scheduler and starting all its jobs.
     member this.Start isParallel =
-        if jobs.Count = 0 then ()
-        else
+        if jobs.Count = 0 then
             ()
+        else
+            while true do
+                //let now = DateTimeOffset.Now
+                for job in jobs do
+                    if Math.Round((job.CurrentDate - DateTimeOffset.Now).TotalSeconds) = 0 then
+                        //job.Execute() |> ignore
+                        printfn "[Execute]"
+                    else
+                        printfn $"[{DateTimeOffset.Now}] => {job.CurrentDate}"
+                    ()
+                (*let now = DateTimeOffset.Now
+                for job in jobs do
+                    if job.CurrentDate = now then
+                        job.Execute() |> Async.AwaitTask |> Async.RunSynchronously
+                    else
+                        printfn $"{now} | {job.CurrentDate} => {(now - job.CurrentDate).TotalSeconds}"*)
+                Thread.Sleep(1000)
 
     member this.Stop() = ()
